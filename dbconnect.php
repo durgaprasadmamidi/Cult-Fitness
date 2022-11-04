ace69c19dfd074982273310545545b3626890677
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-04 16:55:40 -0700

    select query

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,43 @@
+<html>
+<body>
+<?php 
+$username = "durga"; 
+$password = "123"; 
+$database = "cult"; 
+$mysqli = new mysqli("localhost", $username, $password, $database); 
+$query = "SELECT * FROM users";
 
-<?php
-$servername = "localhost";
-$username = "durga";
-$password = "123";
-$dbname = "cult";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+echo '<table border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">Value1</font> </td> 
+          <td> <font face="Arial">Value2</font> </td> 
+          <td> <font face="Arial">Value3</font> </td> 
+          <td> <font face="Arial">Value4</font> </td> 
+          <td> <font face="Arial">Value5</font> </td> 
+      </tr>';
 
-// Check connection
-/*if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
- echo "Connected successfully";*/
-?>`
+if ($result = $mysqli->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["col1"];
+        $field2name = $row["col2"];
+        $field3name = $row["col3"];
+        $field4name = $row["col4"];
+        $field5name = $row["col5"]; 
+
+        echo '<tr> 
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+                  <td>'.$field3name.'</td> 
+                  <td>'.$field4name.'</td> 
+                  <td>'.$field5name.'</td> 
+              </tr>';
+    }
+    $result->free();
+} 
+?>
+</body>
+</html>
+
+
+
