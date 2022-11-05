7f622a3918b1ac05946b6a744fa521edef396b92
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-04 19:32:18 -0700

    adding userdisplay.php

---
+++ search.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+<form action="index.php#users" method="post">
+					<input type="text" name="name" placeholder="Name">
+                      <h3>OR</h3>
+					<input type="text" name="email" placeholder="Email">
+                      <h3>OR</h3>
+                    <input type="text" name="phone" placeholder="Phone">
+                    <input type="submit" name="search" class="login loginmodal-submit" value="Search">
+				  </form>
+
+       
+
+
+    
+    
+</body>
+</html>
---
+++ userdisplay.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<?php require "dbconnect.php"?>
+<?php 
+
+
+echo '<table border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">First Name</font> </td> 
+          <td> <font face="Arial">Last Name</font> </td> 
+          <td> <font face="Arial">E mail</font> </td> 
+          <td> <font face="Arial">Home Phone</font> </td> 
+          <td> <font face="Arial">Cell Phone</font> </td> 
+      </tr>';
+
+if ($result = $mysqli->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["FirstName"];
+        $field2name = $row["LastName"];
+        $field3name = $row["Email"];
+        $field4name = $row["HomePhone"];
+        $field5name = $row["CellPhone"]; 
+
+        echo '<tr> 
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+                  <td>'.$field3name.'</td> 
+                  <td>'.$field4name.'</td> 
+                  <td>'.$field5name.'</td> 
+              </tr>';
+    }
+    $result->free();
+} 
+?>
+</body>
+</html>
+
+
+
