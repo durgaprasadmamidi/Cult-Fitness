e6c3ef87b0fa6e9f11e75eaf4150ff99f2199dc0
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-04 22:02:45 -0700

    fixing

--- adduser.php
+++ adduser.php
@@ -1,7 +1,7 @@
 <?php require "dbconnect.php"?>
 <?php 
 
-    $sql = "INSERT INTO users (firstname,lastname,email,homeaddress,homephone,cellphone) VALUES ('".$_POST["firstname"]."', '".$_POST["lastname"]."', '".$_POST["email"]."', '".$_POST["homeaddress"]."', '".$_POST["homephone"]."', '".$_POST["cellphone"]."')";
+    $sql = "INSERT INTO users (firstname,lastname,email,homephone,cellphone) VALUES ('".$_POST["firstname"]."', '".$_POST["lastname"]."', '".$_POST["email"]."', '".$_POST["homephone"]."', '".$_POST["cellphone"]."')";
     $conn->query($sql);
     $conn->close();
     header("location: index.php");
--- newuser.php
+++ newuser.php
@@ -17,7 +17,7 @@
 					<input type="text" name="firstname" placeholder="First Name">
 					<input type="password" name="lastname" placeholder="Last Name">
                     <input type="text" name="email" placeholder="Email">
-                    <input type="text" name="homeaddress" placeholder="Home Address">
+                    
                     <input type="text" name="homephone" placeholder="Home Phone">
                     <input type="text" name="cellphone" placeholder="Cell Phone">
 					<input type="submit" name="add" class="login loginmodal-submit" value="Add">
