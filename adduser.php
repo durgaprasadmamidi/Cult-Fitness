5f50237a5d4baac3aae93f85b1864aea3a5c5f92
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-04 22:09:34 -0700

    fixing

--- adduser.php
+++ adduser.php
@@ -2,8 +2,8 @@
 <?php 
 
     $sql = "INSERT INTO users (firstname,lastname,email,homephone,cellphone) VALUES ('".$_POST["firstname"]."', '".$_POST["lastname"]."', '".$_POST["email"]."', '".$_POST["homephone"]."', '".$_POST["cellphone"]."')";
-    $conn->query($sql);
-    $conn->close();
+    $mysqli->query($sql);
+    $mysqli->close();
     header("location: index.php");
     
 ?>
