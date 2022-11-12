5570cb9fbfe2993ff34a9721d533658216b81edf
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-11 21:38:30 -0800

    adding users from more companies

--- curls.php
+++ curls.php
@@ -1,10 +1,12 @@
 
 <?php
+echo "Users from parthsaraiya.tech";
 $curl_handle = curl_init();
 curl_setopt($curl_handle, CURLOPT_URL, "http://parthsaraiya.tech/api.php");
 curl_setopt($curl_handle, CURLOPT_HEADER, 0);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
 $contents = curl_exec($curl_handle);
+
 
 curl_close($curl_handle);
 
@@ -12,4 +14,20 @@ foreach (explode(",",$contents) as $content) {
     echo $content."<br/>";
 }
 
+echo "Users from ankursingh.me";
+$curl_handle = curl_init();
+curl_setopt($curl_handle, CURLOPT_URL, "http://ankursingh.me/api.php");
+curl_setopt($curl_handle, CURLOPT_HEADER, 0);
+curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
+$contents = curl_exec($curl_handle);
+
+
+curl_close($curl_handle);
+
+foreach (explode(",",$contents) as $content) {
+    echo $content."<br/>";
+}
+
+
  ?>
+ 
