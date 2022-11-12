c4f4c92899bf6ae73463687652130313284822f4
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-11 21:29:29 -0800

    adding curl

--- curls.php
+++ curls.php
@@ -1,34 +1,47 @@
 <html>
-    
-<body>
-<section class="main-section alabaster" id="tops">
-	<div class="container fullsize">
-<?php
 
+<body>
+<?php
 $curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, "http://esp.durgaprasad.live:4000/expose.php");
+curl_setopt($curl_handle, CURLOPT_URL, "http://parthsaraiya.tech/api.php");
 curl_setopt($curl_handle, CURLOPT_HEADER, 0);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
 $contents = curl_exec($curl_handle);
-curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-$contents = $contents.",".curl_exec($curl_handle);
-curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-$contents = $contents.",".curl_exec($curl_handle);
-curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-$contents = $contents.",".curl_exec($curl_handle);
-curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-$contents = $contents.",".curl_exec($curl_handle);
+// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
+// $contents = $contents.",".curl_exec($curl_handle);
+// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
+// $contents = $contents.",".curl_exec($curl_handle);
+// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
+// $contents = $contents.",".curl_exec($curl_handle);
+// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
+// $contents = $contents.",".curl_exec($curl_handle);
 
-#echo "<br/>";
+
 curl_close($curl_handle);
 
 foreach (explode(",",$contents) as $content) {
     echo $content."<br/>";
 }
 
-?>
+// ?>
         
-    </div>
-</section>
-</body>
-</html>
+//     </div>
+// </section>
+// </body>
+// </html>
+
+
+// <?php
+// $curl_handle = curl_init();
+//                     curl_setopt($curl_handle, CURLOPT_URL, "http://ankursingh.me/api.php");
+//                     curl_setopt($curl_handle, CURLOPT_HEADER, 0);
+//                     curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
+//                     $contents = curl_exec($curl_handle);
+                   
+//                     curl_close($curl_handle);
+                    
+//                     foreach (explode(",",$contents) as $content) {
+//                         echo $content."<br/>";
+//                     }
+
+//                     ?>
