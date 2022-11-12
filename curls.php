527625270c43eb9242f1d2c91f836bdecb38ece8
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-11-11 21:32:57 -0800

    curl final

--- curls.php
+++ curls.php
@@ -1,21 +1,10 @@
-<html>
 
-<body>
 <?php
 $curl_handle = curl_init();
 curl_setopt($curl_handle, CURLOPT_URL, "http://parthsaraiya.tech/api.php");
 curl_setopt($curl_handle, CURLOPT_HEADER, 0);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
 $contents = curl_exec($curl_handle);
-// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-// $contents = $contents.",".curl_exec($curl_handle);
-// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-// $contents = $contents.",".curl_exec($curl_handle);
-// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-// $contents = $contents.",".curl_exec($curl_handle);
-// curl_setopt($curl_handle, CURLOPT_URL, "http://esp.sujith.live/main/expose.php");
-// $contents = $contents.",".curl_exec($curl_handle);
-
 
 curl_close($curl_handle);
 
@@ -23,25 +12,4 @@ foreach (explode(",",$contents) as $content) {
     echo $content."<br/>";
 }
 
-// ?>
-        
-//     </div>
-// </section>
-// </body>
-// </html>
-
-
-// <?php
-// $curl_handle = curl_init();
-//                     curl_setopt($curl_handle, CURLOPT_URL, "http://ankursingh.me/api.php");
-//                     curl_setopt($curl_handle, CURLOPT_HEADER, 0);
-//                     curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
-//                     $contents = curl_exec($curl_handle);
-                   
-//                     curl_close($curl_handle);
-                    
-//                     foreach (explode(",",$contents) as $content) {
-//                         echo $content."<br/>";
-//                     }
-
-//                     ?>
+ ?>
