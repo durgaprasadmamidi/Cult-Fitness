8bf7b0808456f3f89676bc11f67d644a63fccef0
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-12-08 21:12:06 -0800

    more fixes

--- cardio6.php
+++ cardio6.php
@@ -29,7 +29,25 @@
     </head>
     
     <body> 
-        <h1> This is page for Cardio - 6
+        <h1 class="mb-1">  Cardio - 6
         </h1>
+        <div class="mt-5 mt-lg-0 col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="class-thumb">
+                                    <img src="images/class/cardio-class-6.jpg" class="img-fluid" alt="Class">
+                                    <a href="cardio6.php">
+                                    <div class="class-info">
+                                        <h3 >Cardio - 6</h3>
+
+                                        <span><strong>Trained by</strong> - Rishab</span>
+
+                                        <span class="class-price">$75</span>
+
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                       
+                                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
     </body>
 </html>
