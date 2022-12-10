7398284bd58726c360417ff17b5292efd46cf30f
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-12-10 14:21:18 -0800

    added api_products

---
+++ api_products.php
@@ -0,0 +1,19 @@
+<?php 
+
+require "dbconnect.php";
+
+$sql = "SELECT * FROM products ORDER BY hits DESC";
+$results = $mysqli->query($sql);
+$output = [];
+for($i=0; $i<5; $i++){
+    $row = $results->fetch_assoc();
+    $record = array();
+    $record['service_name'] = $row["name"];
+    $record['service_url'] = $row["url"];
+    $record['hits'] = $row["hits"];
+    array_push($output, $record);
+}
+
+echo json_encode($output);
+
+?>
